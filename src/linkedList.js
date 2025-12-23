19970607717ca314436f5c21ccdeec190e68229a
Michael <100490812+DebSol11@users.noreply.github.com>
2025-12-23 20:18:23 +0400

    Rework the starting point of the project

--- src/linkedList.js
+++ src/linkedList.js
@@ -1,35 +1,18 @@
-
-// factory function
-function linkedList (name) {
-  const discordName = "@" + name;
-
-  let reputation = 0;
-  const getReputation = () => reputation;
-  const giveReputation = () => reputation++;
-
-  return { name, discordName, getReputation, giveReputation };
-}
-// and that's very similar, except since it's just a function,
-// we don't need a new keyword
-
 class Node {
-    value = null;
-    nextNode = null;
-
   constructor(value, nextNode) {
-    this.value = null;
-    this.nextNode = null;
+    this.value = null; // Holds the data
+    this.nextNode = null; // Points to the next node 
   }
-
-  method() {} // method
-
-  get something() {} // getter method
-  set something(oneParameter) {} // setter method
-
-  [Symbol.iterator]() {} // method with computed name (symbol here)
-  // ...
 }
 
-// Usage:
-let user = new User("John");
-user.sayHi();
+class LinkedList {
+    constructor() {
+        this.head = null; // The first node in the list
+    }
+    // Method to append a new node to the list
+    append(value) {
+        
+    }
+//   get something() {} // getter method
+//   set something(oneParameter) {} // setter method
+}
