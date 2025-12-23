e0757c52e6efcc1b037a2d95c4adccaf1a19e2d5
Michael <100490812+DebSol11@users.noreply.github.com>
2025-12-23 21:16:58 +0400

    Add printList() method

--- src/linkedList.js
+++ src/linkedList.js
@@ -1,26 +1,37 @@
 class Node {
   constructor(value, nextNode) {
     this.value = null; // Holds the data
-    this.nextNode = null; // Points to the next node 
+    this.nextNode = null; // Points to the next node
   }
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null; // The first node in the list
+  constructor() {
+    this.head = null; // The first node in the list
+  }
+  // Method to append a new node to the list
+  append(value) {
+    const newNode = new Node(value);
+    // The following line checks if this.head is falsy. This includes checking if (this.head === "false" || this.head === "0" || this.head === "" || this.head === "null" || this.head === "undefined" || this.head === "NaN") or short (!this.head)
+    if (!this.head) {
+      this.head = newNode; // If the list is empty, set head to new node
+      return;
     }
-    // Method to append a new node to the list
-    append(value) {
-        const newNode = new Node(value);
-        // The following line checks if this.head is falsy. This includes checking if (this.head === "false" || this.head === "0" || this.head === "" || this.head === "null" || this.head === "undefined" || this.head === "NaN") or short (!this.head)
-        if (!this.head) {
-            this.head = newNode; // If the list is empty, set head to new node 
-            return;
-        }
-        let current = this.head;
-        while (current.newNode) {
-            current = current.nextNode; // move to the last node 
-        }
-        current.nextNode = newNode; // Link the last node to the new node 
+    let current = this.head;
+    while (current.newNode) {
+      current = current.nextNode; // move to the last node
     }
+    current.nextNode = newNode; // Link the last node to the new node
+  }
+
+  // Method to print the list
+  printList() {
+    let current = this.head;
+    let result = "";
+    while (current) {
+      result += current.value + " -> ";
+      current = current.nextNode; // Move to the next node
+    }
+    console.log(result + "null"); // End of the list
+  }
 }
